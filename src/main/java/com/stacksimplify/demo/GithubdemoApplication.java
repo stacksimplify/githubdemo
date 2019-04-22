2e8630065a533d98c67e58f00a55ca65b116b258
Kalyan Reddy <31557463+dkalyanreddy@users.noreply.github.com>
2019-04-22 17:48:08 +0530

    First Commit

---
+++ src/main/java/com/stacksimplify/demo/GithubdemoApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/demo/GithubdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stacksimplify.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
